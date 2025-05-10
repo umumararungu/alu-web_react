e890911f2fdca620a31498f0c78226a0ceb541ca
umumararungu <umcynth@gmail.com>
2025-05-10 23:40:18 +0200

    all

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function() {
+  // Add elements to the page
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  // Counter functionality
+  let count = 0;
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  // Bind debounced click handler
+  $('button').on('click', _.debounce(updateCounter, 300));
+});
