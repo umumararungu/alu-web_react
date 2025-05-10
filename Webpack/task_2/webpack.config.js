44c4586534f94403292288841784e252d60f2759
umumararungu <umcynth@gmail.com>
2025-05-11 00:00:47 +0200

    done

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './js/dashboard_main.js',
@@ -9,15 +7,11 @@ module.exports = {
     path: path.resolve(__dirname, 'public')
   },
   mode: 'production',
-  performance: {
-    maxAssetSize: 500000,
-    maxEntrypointSize: 500000
-  },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
@@ -42,23 +36,11 @@ module.exports = {
               pngquant: {
                 quality: [0.65, 0.90],
                 speed: 4
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75
               }
             }
           }
         ]
       }
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'main.css'
-    }),
-    new CssMinimizerPlugin()
-  ]
+  }
 };
