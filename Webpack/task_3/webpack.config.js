37811fc8e450427e39d74d3f72034d8e3a539cb0
umumararungu <umcynth@gmail.com>
2025-05-12 13:58:11 +0200

    oo

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -5,9 +5,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    header: './header.js',
-    body: './body.js',
-    footer: './footer.js',
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
   },
   output: {
     filename: '[name].bundle.js',
